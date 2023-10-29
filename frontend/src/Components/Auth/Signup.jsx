4b497d12e3c8d330792f3b517dc156072278edb4
Somya <isomya13@gmail.com>
2023-10-29 20:20:14 +0530

    signin signup connected to landing page

--- frontend/src/Components/Auth/Signup.jsx
+++ frontend/src/Components/Auth/Signup.jsx
@@ -38,24 +38,47 @@ export const Signup = () => {
   return (
     <>
       <div>
-      <input
-          placeholder="Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-         <input
-          placeholder="College"
-          onChange={(e) => setCollege(e.target.value)}
-        />
-        <input
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          placeholder="Password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={signup}>Sign Up</button>
+     
+
+
+
+
+
+<form className="form" style={{marginTop:"3vh", width:"100%", height:"fit-content", position:"relative"}}>
+            <div className="nameInput" style={{width:"fit-content", height:"fit-content", backgroundColor:"white", position:"absolute", left:"1vw", fontSize:"0.9rem", color:"#757575"}}> Your Name </div>
+            <input className="nameInput"
+              type="text"
+              name="Name"
+              onChange={(e) => setName(e.target.value)}
+              // placeholder="Last Name"
+              // value={this.state.lastName}
+              // onChange={this.handleChange}
+              required
+              style={{width:"100%", height:"6vh", borderRadius:"5px", border:"1px solid #757575", padding:"0.5vh", marginTop:"1vh", fontSize:"1.05rem", paddingLeft:"0.7vw"}}></input>
+            <div className="emailInput" style={{width:"fit-content", height:"fit-content", backgroundColor:"white", position:"absolute", left:"1vw", fontSize:"0.9rem", color:"#757575", top:"8vh"}}> Email </div>
+            <input className="emailInput" 
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            // placeholder="Email Address"
+            // value={this.state.email}
+            // onChange={this.handleChange}
+            required
+            style={{width:"100%", height:"6vh", borderRadius:"5px", border:"1px solid #757575", padding:"0.5vh", marginTop:"2vh", fontSize:"1.05rem", paddingLeft:"0.7vw"}}></input>
+            <div className="passwordInput"style={{width:"fit-content", height:"fit-content", backgroundColor:"white", position:"absolute", left:"1vw", fontSize:"0.9rem", color:"#757575", top:"16vh"}}>Password</div>
+            <input className="passwordInput" 
+              type="password"
+              name="password"
+              // placeholder="Password"
+              // value={this.state.password}
+              // onChange={this.handleChange}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              style={{width:"100%", height:"6vh", borderRadius:"5px", border:"1px solid #757575", padding:"0.5vh", marginTop:"2vh", fontSize:"1.05rem", paddingLeft:"0.7vw"}}></input>
+            <button type='submit' className='signUp' style={{marginTop:"1.5vh",border:"1px solid black", width:"100%", height:"6vh", display:"flex", padding:"0.5vh", borderRadius:"5px", backgroundColor:"black", position:"relative", color:"white", fontWeight:"500",fontSize:"1rem", justifyContent:"center", alignItems:"center", cursor:"pointer"}}
+            onClick={signup}
+            >SIGNUP</button>
+          </form>
       </div>
     </>
   );
--- frontend/src/Components/LandingPage/LandingPage.jsx
+++ frontend/src/Components/LandingPage/LandingPage.jsx
@@ -4,92 +4,61 @@ import { db } from '../../../Firebase/firebase'
 import {getDocs, collection} from "firebase/firestore"
 // import {introImg} from '../../assets/landing_page/intro1.png';
 import './LandingPage.css'
+import { SignIn } from '../Auth/Signin'
+import { Signup } from '../Auth/Signup'
+
+
 
 
 const LandingPage = () => {
+  const  currentUser = auth?.currentUser
 
-  const [users, setUsers] = useState([]);
+  
+  const [toggleState, setToggleState] = useState('signup');
 
-  const usersCollection = collection(db, "users")
-
-  useEffect(()=>{
-    const getUsers= async()=>{
-
-         try {
-          const data = await getDocs(usersCollection)
-          const res=data.docs.map((doc)=> (
-            {
-              ...doc.data(), id:doc.id
-            }
-          ))
-         
-      
-         } catch (error) {
-            alert(error)
-         }
-       
-    }
-
-    getUsers()
-  }, [])
+  const toggleComponent = () => {
+    setToggleState(toggleState === 'signin' ? 'signup' : 'signin');
+  };
 
   
   return (
     <div className='outer-landing' style={{width: "100vw", overflow: "none"}}>
+      
     {/* <div>hello {auth?.currentUser?.email}</div> */}
     <section className="Intro" style={{width:"100%", height:"110vh", backgroundColor:"#242424"}}>
+  
       <div className="intro-box" style={{width:"90%", height:"80%", display:"flex", justifyContent:"space-between", overflow:"hidden" ,alignItems:"center", top:"50vh", left:"50vw", position:"absolute", transform:"translate(-50%,-50%)"}}>
+     
         <div className="introtext" style={{width:"35vw", height:"fit-content", backgroundColor:"white", borderRadius:"15px", marginTop:"8vh", marginLeft:"1vw", padding:"4vh 4vw"}}>
-          <div className="introTitle" style={{fontWeight:"300", color:"black"}}>Lets Get You Started</div>
-          <div className="formTitle" style={{fontWeight:"500",fontSize:"2em", color:"black", marginTop:"3vh"}}>Create an Account</div>
-          <form className="form" style={{marginTop:"3vh", width:"100%", height:"fit-content", position:"relative"}}>
-            <div className="nameInput" style={{width:"fit-content", height:"fit-content", backgroundColor:"white", position:"absolute", left:"1vw", fontSize:"0.9rem", color:"#757575"}}> Your Name </div>
-            <input className="nameInput"
-              type="text"
-              name="Name"
-              // placeholder="Last Name"
-              // value={this.state.lastName}
-              // onChange={this.handleChange}
-              required
-              style={{width:"100%", height:"6vh", borderRadius:"5px", border:"1px solid #757575", padding:"0.5vh", marginTop:"1vh", fontSize:"1.05rem", paddingLeft:"0.7vw"}}></input>
-            <div className="emailInput" style={{width:"fit-content", height:"fit-content", backgroundColor:"white", position:"absolute", left:"1vw", fontSize:"0.9rem", color:"#757575", top:"8vh"}}> Email </div>
-            <input className="emailInput" 
-            type="email"
-            name="email"
-            // placeholder="Email Address"
-            // value={this.state.email}
-            // onChange={this.handleChange}
-            required
-            style={{width:"100%", height:"6vh", borderRadius:"5px", border:"1px solid #757575", padding:"0.5vh", marginTop:"2vh", fontSize:"1.05rem", paddingLeft:"0.7vw"}}></input>
-            <div className="passwordInput"style={{width:"fit-content", height:"fit-content", backgroundColor:"white", position:"absolute", left:"1vw", fontSize:"0.9rem", color:"#757575", top:"16vh"}}>Password</div>
-            <input className="passwordInput" 
-              type="password"
-              name="password"
-              // placeholder="Password"
-              // value={this.state.password}
-              // onChange={this.handleChange}
-              required
-              style={{width:"100%", height:"6vh", borderRadius:"5px", border:"1px solid #757575", padding:"0.5vh", marginTop:"2vh", fontSize:"1.05rem", paddingLeft:"0.7vw"}}></input>
-            <button type='submit' className='signUp' style={{marginTop:"1.5vh",border:"1px solid black", width:"100%", height:"6vh", display:"flex", padding:"0.5vh", borderRadius:"5px", backgroundColor:"black", position:"relative", color:"white", fontWeight:"500",fontSize:"1rem", justifyContent:"center", alignItems:"center", cursor:"pointer"}}>GET STARTED</button>
-          </form>
-          <div className="break" style={{marginTop:"1.5vh",marginBottom:"1.5vh", display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-
-          <div className="line" style={{backgroundColor:"#E0E0E0", height:"1px", width:"40%"}}></div>
-Or
-          <div className="line" style={{backgroundColor:"#E0E0E0", height:"1px", width:"40%"}}></div>
-          </div>
-          <button className='googleSignUp' style={{border:"1px solid black", width:"100%", height:"6vh", display:"flex", padding:"0.5vh", borderRadius:"5px", backgroundColor:"white", position:"relative"}}></button>
-          <button className='googleSignUp' style={{marginTop:"1.5vh",border:"1px solid black", width:"100%", height:"6vh", display:"flex", padding:"0.5vh", borderRadius:"5px", backgroundColor:"white", position:"relative"}}></button>
-          <div className="loginText" style={{justifyContent:"center", display:"flex",marginTop:"1.5vh"}}>Already have an account?
-          <a href="/signin" style={{color:"black",fontWeight:"500", marginLeft:"0.5vw"}}>LOGIN HERE</a>
-          </div>
+          {/* <div className="introTitle" style={{fontWeight:"300", color:"black"}}>Lets Get You Started</div>
+          <div className="formTitle" style={{fontWeight:"500",fontSize:"2em", color:"black", marginTop:"3vh"}}>
+            Welcome to Campus Connect
+          </div> */}
+      {
+        !currentUser && <div>
+        <button 
+        style={{
+          padding: "5px",
+          backgroundColor: "black",
+          color: "white",
+          fontSize: "24px"
+        }}
+        
+        onClick={toggleComponent}>{toggleState === 'signup' ? "Signin instead" : "Signup instead"}</button>
+        {toggleState === 'signin' && <SignIn/>}
+        {toggleState === 'signup' && <Signup />}
+      </div>
+      }    
+        
+ 
         </div>
+        
         <div className="introimg" style={{width:"50vw", height:"40vw",
         //  backgroundColor:"#FFE8B6",
           borderRadius:"25px", position:"relative", justifyContent:"center", alignItems:"center", padding:"4vw"}}>
           {/* <Image src={introImg} style={{width:"100%", height:"100%", objectFit:"cover", zIndex:"10"}}/> */}
           <div className="introRightTitle" style={{display:"flex", flexDirection:"column", alignItems:"flex-end",  marginRight: "16vh"}}>
-            <h1 style={{paddingTop: "18vh", color:"white", width:"100%", fontSize:"2rem", fontWeight:"bolder"}}>Reimagine colleges as hubs of <span style={{color:"#FFB2C1"}}>exploration</span>, <span style={{color:"#FFB800"}}>creativity</span>, and <span style={{color:"#00CBFE"}}>growth</span>.</h1>
+            <h1 style={{ color:"white", width:"100%", fontSize:"2rem", fontWeight:"bolder"}}>Reimagine colleges as hubs of <span style={{color:"#FFB2C1"}}>exploration</span>, <span style={{color:"#FFB800"}}>creativity</span>, and <span style={{color:"#00CBFE"}}>growth</span>.</h1>
             <h4 style={{color:"white", width:"40%", marginTop:"2vh", fontWeight:"lighter"}}>with Campus Connect</h4>
           </div>
           <img src={`https://drive.google.com/uc?id=1iS6iNzILFH0dBy6lkLFMmbDf91mt2rIn`} style={{width:"100%", height:"100%", objectFit:"cover", zIndex:"10"}}/>
